71fdabc11fba27f759767f4ce3982a99b617280d
samiviro <samiravillalobos05@gmail.com>
2026-03-25 16:07:04 -0600

    web

---
+++ Vehiculos.WEB/Abstracciones/Modelos/Marca.cs
@@ -0,0 +1,8 @@
+﻿namespace Abstracciones.Modelos
+{
+    public class Marca
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ Vehiculos.WEB/Abstracciones/Modelos/Modelo.cs
@@ -0,0 +1,9 @@
+﻿namespace Abstracciones.Modelos
+{
+    public class Modelo
+    {
+        public Guid Id { get; set; }
+        public Guid IdMarca { get; set; }
+        public string Nombre { get; set; }
+    }
+}
